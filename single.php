9edeb3371333662584a169a77d6156caf13cfd40
Kev Quirk <kev@quirk.cc>
2020-06-19 12:00:57 +0100

    Comment guidelines

--- single.php
+++ single.php
@@ -47,7 +47,7 @@ get_header();
 		<hr>
 
 		<h2><i class="lar la-comment la-lg"></i> Comments</h2>
-		<p>Please read my <a target="blank" href="/comment-guidelines">comment guidelines</a> before posting a comment.</p>
+		<p class="entry-meta"><b>Please read my <a target="blank" href="/comment-guidelines">comment guidelines</a> before posting a comment.</b></p>
 
 		<?php
 			// If comments are open or we have at least one comment, load up the comment template.
