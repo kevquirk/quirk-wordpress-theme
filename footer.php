de3cddd40e64436a90e8b135149fc3d715d78311
Kev Quirk <kev@pop-os.localdomain>
2020-08-16 11:35:54 +0100

    Added weird wide webring

--- footer.php
+++ footer.php
@@ -11,7 +11,13 @@
 	</div>
 
 	<footer id="colophon">
-		<p>All work licensed under <i class="la la-creative-commons la-lg"></i><i class="lab la-creative-commons-by la-lg"></i><i class="lab la-creative-commons-sa la-lg"></i> unless otherwise stated.
+			<p class="webring">This site is part of <a href="https://weirdwidewebring.net">The Weird Wide Webring</a><br>
+			<a href="https://weirdwidewebring.net/prev.html">Previous site</a>  |
+			<a href="https://weirdwidewebring.net/next.html">Next site</a>  |
+			<a href="https://weirdwidewebring.net/random.html">Random site</a>  |
+			<a href="https://weirdwidewebring.net">All sites</a></p>
+
+			<p>All work licensed under <i class="la la-creative-commons la-lg"></i><i class="lab la-creative-commons-by la-lg"></i><i class="lab la-creative-commons-sa la-lg"></i> unless otherwise stated.
 		<br><a href="/disclaimer">Disclaimer</a> | <a href="/license-information">License Info</a> | <a href="/privacy">Privacy</a> | <a href="/buy-me-a-coffee">Coffee?</a></p>
 		<p><a class="menu" href="#top"><i class="las la-arrow-up la-lg"></i> Back To Top <i class="las la-arrow-up la-lg"></i></a></p>
 	</footer>
