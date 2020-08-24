17058ca214b58826cab34884f7b59a00557f192a
Kev Quirk <kev@pop-os.localdomain>
2020-08-24 20:42:33 +0100

    Added up arrow to top link

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
 	<footer id="colophon">
 			<p>All work licensed under <a target="blank" href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a> unless otherwise stated.
 		<br><a href="/disclaimer">Disclaimer</a> | <a href="/license-information">License Info</a> | <a href="/privacy">Privacy</a> | <a href="/buy-me-a-coffee">Coffee?</a></p>
-		<p><a class="menu" href="#top"><i class="las la-arrow-up la-lg"></i> Back To Top <i class="las la-arrow-up la-lg"></i></a></p>
+		<p><a class="menu" href="#top"><i class="fa fa-arrow-up" aria-hidden="true"></i> Back To Top <i class="fa fa-arrow-up" aria-hidden="true"></i></a></p>
 	</footer>
 </div>
 
