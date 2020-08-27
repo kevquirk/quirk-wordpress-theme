d323867df3b559c67e6d3d15b3fbaec6523e8d98
Kev Quirk <kev@pop-os.localdomain>
2020-08-27 21:33:55 +0100

    Spacing

--- footer.php
+++ footer.php
@@ -11,8 +11,10 @@
 	</div>
 
 	<footer id="colophon">
-			<p>All work licensed under <b>CC BY-SA 4.0</b> unless otherwise stated.
-		<br><a href="/disclaimer">Disclaimer</a> | <a href="/license-information">License Info</a> | <a href="/privacy">Privacy</a> | <a href="/buy-me-a-coffee">Coffee?</a></p>
+		<p>All work licensed under <b>CC BY-SA 4.0</b> unless otherwise stated.<br>
+
+		<a href="/disclaimer">Disclaimer</a> | <a href="/license-information">License Info</a> | <a href="/privacy">Privacy</a> | <a href="/buy-me-a-coffee">Coffee?</a></p>
+		
 		<p><a class="menu" href="#top"><i class="fa fa-arrow-up" aria-hidden="true"></i> Back To Top <i class="fa fa-arrow-up" aria-hidden="true"></i></a></p>
 	</footer>
 </div>
