0a7535f5c21d977d0c2f1400b0c38e8dc819d156
Kev Quirk <kev@quirk.cc>
2020-09-22 08:32:50 +0100

    Moved h-card to footer

--- footer.php
+++ footer.php
@@ -16,6 +16,35 @@
 		<a href="/disclaimer">Disclaimer</a> | <a href="/license-information">License Info</a> | <a href="/privacy">Privacy</a> | <a href="/buy-me-a-coffee">Coffee?</a></p>
 
 		<p><a class="menu" href="#top"><i class="fa fa-arrow-up" aria-hidden="true"></i> Back To Top <i class="fa fa-arrow-up" aria-hidden="true"></i></a></p>
+
+		<!-- INDIEWEB H-CARD use this link to verify it's ok - https://indiewebify.me/validate-h-card/ -->
+		<section style="display: none;" class="h-card">
+			<!-- About me -->
+					<span class="p-name">Kev Quirk</span>
+					<span class="p-note">I'm a cyber security professional and privacy advocate from North West England. My interest include drawing, fish keeping, motorbikes & open source software.</span>
+
+			<!-- My Indieweb profile pic -->
+					<img class="u-photo" src="https://cdn.kevq.uk/wp-content/uploads/2019/11/400px-round-grey-glasses.png"/>
+
+			<!-- My location -->
+					<span class="p-locality">North West England</span>
+
+			<!-- Links -->
+					<a class="u-url u-uid" href="https://kevq.uk"></a>
+					<a class="u-email" rel="me" href="mailto:kev@craves.coffee"></a>
+					<a class="u-url" rel="me" href="https://fosstodon.org/@kev"></a>
+					<a class="u-url" rel="me" href="https://twitter.com/kevquirk"></a>
+					<a class="u-url" rel="me" href="https://keybase.io/kevq"></a>
+
+			<!-- Categories -->
+					<span class="p-category">Blogging</span>
+					<span class="p-category">Fish keeping</span>
+					<span class="p-category">InfoSec</span>
+					<span class="p-category">Motorbikes</span>
+					<span class="p-category">Open Source Software</span>
+					<span class="p-category">Privacy</span>
+					<span class="p-category">Web Design</span>
+		</section>
 	</footer>
 </div>
 
